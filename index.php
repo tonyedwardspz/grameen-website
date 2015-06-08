d0939eec7cddd03522daa1c8b5e1bd776d2b1550
mjcave <mjcplymuni@gmail.com>
2015-06-08 16:13:21 +0100

    1st Change
    create 1st change

--- index.php
+++ index.php
@@ -8,9 +8,9 @@ include 'header.php';
     <h1>Hello, world!</h1>
 
     <p>This is a super simple html page</p>
-	<p> MIKES extra bit for testing Git Hub </p>
-	<p> MIKES Test for BRANCHING</p>
-	
+	  <p> MIKES extra bit for testing Git Hub </p>
+	  <p> Mike :- This branch for development of Grameen Homepage Layout</p>
+      	
 
 
 
