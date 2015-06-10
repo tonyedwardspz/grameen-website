839ae3ac605b395cbaf4aa6ca2e80f4858d2136b
Tony Edwards <tony@plymouthsoftware.com>
2015-06-10 17:07:43 +0100

    Code review changes

--- index.php
+++ index.php
@@ -1,48 +1,51 @@
 <?php
 $metaTag = 'This is an example of a meta description. This will often show up in search results.';
-$title = 'Bootstrap 101'
+$title = 'Bootstrap 101';
 
 include 'header.php';
 ?>
-    <!-- Main jumbotron for a primary marketing message or call to action -->
+
     <div class="jumbotron">
       <div class="container">
         <h1>Welcome to Grameen Tandoori Restaurant</h1>
-        <p style="float: left; padding-right: 10px" ><img src="images/food4homepage.jpg" height="266" width="400" border="1px"></p>
+          <img class="jumboImage" src="images/food4homepage.jpg" height="266" width="400">
           <p>The Grameen Tandoori restaurant is situated on Hayle Terrace overlooking Hayle estuary. The unassuming exterior opens up into a wonderful and contemporary Indian dining experience.</p>
           <p>The restaurant offers excellent Bangladeshi Tandoori food as both a sit down meal and takeaway. 
              All food is prepared by our highly experienced team using only the freshest ingredients and the best spices.</p>
           <p>"The best, and most reliable, Indian restaurant in the town. A modern interior and discrete lighting make a pleasant atmosphere." - www.gourmetbritain.com</p>
-        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>  <!-- MJC TODO - point this to relevant page -->
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a>
       </div>
     </div>
 
     <div class="container">
-      <!-- Example row of columns -->
       <div class="row">
+
         <div class="col-md-4">
           <h2>Place an Order</h2>
           <p>If you know what you would like and wish to book a table or place an order for takeaway, the restaurant can be contacted directly on 01736 758470.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <a class="btn btn-default" href="#" role="button">View details &raquo;</a>
         </div>
+
         <div class="col-md-4">
           <h2>Opening Hours</h2>
           <p>Open 7 Days a week including Bank Holidays</p>
           <p>Lunch - 12.00pm to 2.00pm<br>Evening - 5.30pm to 11.30pm</p>
           <p>The Grameen Restaurant has disabled facilities</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <a class="btn btn-default" href="#" role="button">View details &raquo;</a>
        </div>
+
         <div class="col-md-4">
           <h2>What our Customers say</h2>
           <p>"The best, and most reliable, Indian restaurant in the town. A modern interior and discrete lighting make a pleasant atmosphere." - www.gourmetbritain.com</p>
           <p>"Everybody’s food was was delicious. I had the vegetable biryani which was fragrant and tasty and my daughter enjoyed the vegetable balti. 
             All the dishes were beautifully presented and the waiters were attentive, but never pushy. They do take-aways too!" - www.qype.co.uk </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <a class="btn btn-default" href="#" role="button">View details &raquo;</a>
         </div>
+
       </div>
 
       <hr>
-      <p>All major cards accepted. Sorry but we not accept Cheques &nbsp;&nbsp; <img src="images/credit_card_accepted.jpg" border="1px"></p>
+      <p>All major cards accepted. <img src="images/credit_card_accepted.jpg" border="1px"></p>
 
 <?php
 include 'footer.php';
