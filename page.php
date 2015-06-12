213cf08b966059af10607f6dd62830ab074d1666
mjcave <mjcplymuni@gmail.com>
2015-06-12 15:37:20 +0100

    Created sidebar.php
    added sidebar.php and rejigged page.php to use it

--- page.php
+++ page.php
@@ -25,32 +25,10 @@ include 'header.php';
 
       </div><!-- /.blog-main -->
 
-      <!-- SIDEBAR -->
-        <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
-          <div class="sidebar-module sidebar-module-inset">
-            <h4>About</h4>
-            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-          </div>
-          <div class="sidebar-module">
-            <h4>Archives</h4>
-            <ol class="list-unstyled">
-              <li><a href="#">March 2014</a></li>
-              <li><a href="#">February 2014</a></li>
-              <li><a href="#">January 2014</a></li>
-              <li><a href="#">December 2013</a></li>
-            </ol>
-          </div>
-          <div class="sidebar-module">
-            <h4>Elsewhere</h4>
-            <ol class="list-unstyled">
-              <li><a href="#">GitHub</a></li>
-              <li><a href="#">Twitter</a></li>
-              <li><a href="#">Facebook</a></li>
-            </ol>
-          </div>
-        </div><!-- /.blog-sidebar -->
- 
-
+      <?php
+      include 'sidebar.php';
+      ?>
+      
       <!-- FANCYBOX Images -->
       <div class="col-sm-3">
           <a class="fancybox" rel="group" href="images/1_b.jpg"><img src="images/1_s.jpg" alt="pretty pic" /></a>
