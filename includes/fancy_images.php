7d11a7dfa1bc1f9d5a195e67b04bd2fd97367702
mjcave <mjcplymuni@gmail.com>
2015-06-19 17:11:32 +0100

    Fancybox container margin added
    Added fancymargin css class to style.css and applied it to
    fancy_images.php

--- includes/fancy_images.php
+++ includes/fancy_images.php
@@ -1,29 +1,29 @@
 
       <!-- FANCYBOX Images -->
       <div class="row fancy-container" >
+        <div class = "fancymargin">
+          <div class="col-sm-6 col-md-3 fancyborder">
+            <a class="fancybox" rel="group" href="images/1_b.jpg">
+              <img src="images/1_s.jpg" alt="pretty pic" />
+            </a>
+          </div>
 
-        <div class="col-sm-6 col-md-3 fancyborder">
-          <a class="fancybox" rel="group" href="images/1_b.jpg">
-            <img src="images/1_s.jpg" alt="pretty pic" />
-          </a>
-        </div>
+          <div class="col-sm-6 col-md-3 fancyborder">
+            <a class="fancybox" rel="group" href="images/2_b.jpg">
+              <img src="images/2_s.jpg" alt="prettier pic" />
+            </a>
+          </div>
+          
+          <div class="col-sm-6 col-md-3 fancyborder">
+            <a class="fancybox" rel="group" href="images/3_b.jpg">
+              <img src="images/3_s.jpg" alt="pretty pic 3" />
+            </a>
+          </div>
 
-        <div class="col-sm-6 col-md-3 fancyborder">
-          <a class="fancybox" rel="group" href="images/2_b.jpg">
-            <img src="images/2_s.jpg" alt="prettier pic" />
-          </a>
-        </div>
-        
-        <div class="col-sm-6 col-md-3 fancyborder">
-          <a class="fancybox" rel="group" href="images/3_b.jpg">
-            <img src="images/3_s.jpg" alt="pretty pic 3" />
-          </a>
-        </div>
-
-        <div class="col-sm-6 col-md-3 fancyborder">
-          <a class="fancybox" rel="group" href="images/4_b.jpg">
-            <img src="images/4_s.jpg" alt="pretty pic4" />
-          </a>
-        </div>
-
+          <div class="col-sm-6 col-md-3 fancyborder">
+            <a class="fancybox" rel="group" href="images/4_b.jpg">
+              <img src="images/4_s.jpg" alt="pretty pic4" />
+            </a>
+          </div>
+        </div> <!-- fancy margin -->
      </div> <!-- Row -->
