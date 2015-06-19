f077a9d3bd468f62864e9303aae26f7a40ef2c90
Tony Edwards <tony@plymouthsoftware.com>
2015-06-19 22:03:15 +0100

    Add footer template

--- includes/footer.php
+++ includes/footer.php
@@ -1,30 +1,84 @@
-     
-      <hr>  
-      <div class="row">     
-        <div class="col-sm-12">
-          <p>All major cards accepted. <img src="images/credit_card_accepted.jpg" border="1px"></p>
-        </div>
-      </div>
-      
 
-      <footer class="row">
 
-        <div class="col-sm-6">
-			   <p>&copy; <a href="http://purelywebdesign.co.uk/">Purely Web Design <?php echo date('Y'); ?></p> 
-        </div>
+      <hr>
 
-        <div class="col-sm-6">
-          <div class ="alignright">
-             <a href="index.php">Home</a>
-             <a href="restaurant.php">Restaurant</a>
-             <a href="takeaway.php">Takeaway</a>
-             <a href="contact.php">Contact</a>
-             <a href="attractions.php">Local Attractions</a>
-             <a href="privacy.php">Privacy Policy</a>
+      <footer>
+        <div class="row">
+
+          <div class="col-sm-6 col-md-3">
+            <p clas="image-box">
+              <img src="http://placehold.it/500x330" alt="Image alt text">
+            </p>
           </div>
-        </div>
 
-      </footer><!-- end of row-->
+
+          <div class="col-sm-6 col-md-3">
+            <p clas="image-box">
+              <img src="http://placehold.it/500x330" alt="Image alt text">
+            </p>
+          </div>
+
+
+          <div class="col-md-3 col-sm-12">
+            <h3>Pages</h3>
+            <ul>
+              <li>
+                <a href="index.php" title="Grameen Tandoori Home Page">
+                  <i class="fa fa-file"></i> Home</a>
+              </li>
+              <li>
+                <a href="restaurant.php" title="Grameen Restaurant Menu">
+                  <i class="fa fa-file"></i> Our Restaurant</a>
+              </li>
+              <li>
+                <a href="takeaway.php" title="Grameen Takeaway Menu">
+                  <i class="fa fa-file"></i> Takeaways and Delivery</a>
+              </li>
+              <li>
+                <a href="contact.php" title="Order Takeaway and Book Table">
+                  <i class="fa fa-file"></i> Contact us</a>
+              </li>
+              <li>
+                <a href="attractions.php" title="Attractions in West Cornwall">
+                  <i class="fa fa-file"></i> Local Attractions</a>
+              </li>
+            </ul>
+          </div>
+
+
+          <div class="col-lg-3 col-md-6">
+            <h3>Contact:</h3>
+            <p>Have a question or feedback? Contact me!</p>
+            <p>
+              <a href="" title="Contact me!"><i class="fa fa-envelope"></i> 01736 000000</a>
+            </p>
+            
+            <h3>Follow:</h3>
+            <a href="" id="gh" target="_blank" title="Twitter">
+              <span class="fa-stack fa-lg">
+                <i class="fa fa-square-o fa-stack-2x"></i>
+                <i class="fa fa-twitter fa-stack-1x"></i>
+              </span>
+              Twitter
+            </a>
+            <a href="" id="gh" target="_blank" title="Twitter">
+              <span class="fa-stack fa-lg">
+                <i class="fa fa-square-o fa-stack-2x"></i>
+                <i class="fa fa-twitter fa-stack-1x"></i>
+              </span>
+              Twitter
+            </a>
+          </div>
+
+          <hr>
+          <div class="col-sm-12 col-md-6">
+            <p>Copyright © Your Website | <a href="">Privacy Policy</a> | <a href="">Terms of Use</a></p>
+          </div>
+
+          <div class="col-sm-12 col-md-6 alignright">
+            <p>All major cards accepted. <img src="images/credit_card_accepted.jpg"></p>
+          </div>
+        </footer>
 
     </div> <!-- /container -->
 	
