5684547971de0670428e4b077aa0084fd6199533
Tony Edwards <tony@plymouthsoftware.com>
2015-06-20 01:41:12 +0100

    Footer tweaks

--- includes/footer.php
+++ includes/footer.php
@@ -1,84 +1,81 @@
 
+      <footer class="row">
+        <hr>
 
-      <hr>
-
-      <footer>
-        <div class="row">
-
-          <div class="col-sm-6 col-md-3">
-            <p clas="image-box">
-              <img src="http://placehold.it/500x330" alt="Image alt text">
-            </p>
-          </div>
+        <div class="col-sm-6 col-md-3">
+          <p clas="image-box">
+            <img src="http://placehold.it/500x330" alt="Image alt text">
+          </p>
+        </div>
 
 
-          <div class="col-sm-6 col-md-3">
-            <p clas="image-box">
-              <img src="http://placehold.it/500x330" alt="Image alt text">
-            </p>
-          </div>
+        <div class="col-sm-6 col-md-3">
+          <p clas="image-box">
+            <img src="http://placehold.it/500x330" alt="Image alt text">
+          </p>
+        </div>
 
 
-          <div class="col-md-3 col-sm-12">
-            <h3>Pages</h3>
-            <ul>
-              <li>
-                <a href="index.php" title="Grameen Tandoori Home Page">
-                  <i class="fa fa-file"></i> Home</a>
-              </li>
-              <li>
-                <a href="restaurant.php" title="Grameen Restaurant Menu">
-                  <i class="fa fa-file"></i> Our Restaurant</a>
-              </li>
-              <li>
-                <a href="takeaway.php" title="Grameen Takeaway Menu">
-                  <i class="fa fa-file"></i> Takeaways and Delivery</a>
-              </li>
-              <li>
-                <a href="contact.php" title="Order Takeaway and Book Table">
-                  <i class="fa fa-file"></i> Contact us</a>
-              </li>
-              <li>
-                <a href="attractions.php" title="Attractions in West Cornwall">
-                  <i class="fa fa-file"></i> Local Attractions</a>
-              </li>
-            </ul>
-          </div>
+        <div class="col-sm-6 col-md-3">
+          <h3>Pages</h3>
+          <ul>
+            <li>
+              <a href="index.php" title="Grameen Tandoori Home Page">
+                <i class="fa fa-file"></i> Home</a>
+            </li>
+            <li>
+              <a href="restaurant.php" title="Grameen Restaurant Menu">
+                <i class="fa fa-file"></i> Our Restaurant</a>
+            </li>
+            <li>
+              <a href="takeaway.php" title="Grameen Takeaway Menu">
+                <i class="fa fa-file"></i> Takeaways and Delivery</a>
+            </li>
+            <li>
+              <a href="contact.php" title="Order Takeaway and Book Table">
+                <i class="fa fa-file"></i> Contact us</a>
+            </li>
+            <li>
+              <a href="attractions.php" title="Attractions in West Cornwall">
+                <i class="fa fa-file"></i> Local Attractions</a>
+            </li>
+          </ul>
+        </div>
 
 
-          <div class="col-lg-3 col-md-6">
-            <h3>Contact:</h3>
-            <p>Have a question or feedback? Contact me!</p>
-            <p>
-              <a href="" title="Contact me!"><i class="fa fa-envelope"></i> 01736 000000</a>
-            </p>
-            
-            <h3>Follow:</h3>
-            <a href="" id="gh" target="_blank" title="Twitter">
-              <span class="fa-stack fa-lg">
-                <i class="fa fa-square-o fa-stack-2x"></i>
-                <i class="fa fa-twitter fa-stack-1x"></i>
-              </span>
-              Twitter
-            </a>
-            <a href="" id="gh" target="_blank" title="Twitter">
-              <span class="fa-stack fa-lg">
-                <i class="fa fa-square-o fa-stack-2x"></i>
-                <i class="fa fa-twitter fa-stack-1x"></i>
-              </span>
-              Twitter
-            </a>
-          </div>
+        <div class="col-sm-6 col-md-3">
+          <h3>Contact:</h3>
+          <p>Have a question or feedback? Contact me!</p>
+          <p>
+            <a href="" title="Contact me!"><i class="fa fa-envelope"></i> 01736 000000</a>
+          </p>
 
-          <hr>
-          <div class="col-sm-12 col-md-6">
-            <p>&copy; <?php echo date('Y'); ?> Grameen Tandoori | Developed by <a href="http://purelywebdesign.co.uk/">Purely Web Design</a> | <a href="privacy.php">Privacy Policy</a></p>
-          </div>
+          <a href="" target="_blank" title="Twitter">
+            <span class="fa-stack fa-lg">
+              <i class="fa fa-square-o fa-stack-2x"></i>
+              <i class="fa fa-twitter fa-stack-1x"></i>
+            </span>
+          </a>
+          <a href="" target="_blank" title="Twitter">
+            <span class="fa-stack fa-lg">
+              <i class="fa fa-square-o fa-stack-2x"></i>
+              <i class="fa fa-twitter fa-stack-1x"></i>
+            </span>
+          </a>
+        </div>
 
-          <div class="col-sm-12 col-md-6 alignright">
-            <p>All major cards accepted. <img src="images/credit_card_accepted.jpg"></p>
-          </div>
-        </footer>
+        <hr>
+
+        <div class="col-sm-12 col-md-6">
+          <p>&copy; <?php echo date('Y'); ?> Grameen Tandoori | <a href="http://purelywebdesign.co.uk/">Purely Web Design</a> | <a href="privacy.php">Privacy Policy</a></p>
+        </div>
+
+
+        <div class="col-sm-12 col-md-6 alignright">
+          <p>All major cards accepted. <img src="images/credit_card_accepted.jpg"></p>
+        </div>
+
+      </footer>
 
     </div> <!-- /container -->
 	
