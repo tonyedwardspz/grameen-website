5133579d7641c8879a81a9297730fca585b93fd2
Tony Edwards <tony@plymouthsoftware.com>
2015-06-20 01:23:29 +0100

    Tidy up the images folder

--- includes/fancy_images.php
+++ includes/fancy_images.php
@@ -3,26 +3,26 @@
       <div class="row fancy-container" >
 
         <div class="col-sm-6 col-md-3 fancyborder">
-          <a class="fancybox" rel="group" href="images/1_b.jpg">
-            <img src="images/1_s.jpg" alt="pretty pic" />
+          <a class="fancybox" rel="group" href="images/photos/1_b.jpg">
+            <img src="images/photos/1_s.jpg" alt="pretty pic" />
           </a>
         </div>
 
         <div class="col-sm-6 col-md-3 fancyborder">
-          <a class="fancybox" rel="group" href="images/2_b.jpg">
-            <img src="images/2_s.jpg" alt="prettier pic" />
+          <a class="fancybox" rel="group" href="images/photos/2_b.jpg">
+            <img src="images/photos/2_s.jpg" alt="prettier pic" />
           </a>
         </div>
         
         <div class="col-sm-6 col-md-3 fancyborder">
-          <a class="fancybox" rel="group" href="images/3_b.jpg">
-            <img src="images/3_s.jpg" alt="pretty pic 3" />
+          <a class="fancybox" rel="group" href="images/photos/3_b.jpg">
+            <img src="images/photos/3_s.jpg" alt="pretty pic 3" />
           </a>
         </div>
 
         <div class="col-sm-6 col-md-3 fancyborder">
-          <a class="fancybox" rel="group" href="images/4_b.jpg">
-            <img src="images/4_s.jpg" alt="pretty pic4" />
+          <a class="fancybox" rel="group" href="images/photos/4_b.jpg">
+            <img src="images/photos/4_s.jpg" alt="pretty pic4" />
           </a>
         </div>
 
