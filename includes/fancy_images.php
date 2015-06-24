b94578bc4ebd7112b796cba1656ab0472636c609
mjcave <mjcplymuni@gmail.com>
2015-06-24 22:02:15 +0100

    Tidy code in fancyimages

--- includes/fancy_images.php
+++ includes/fancy_images.php
@@ -5,10 +5,8 @@
           <!-- MJC create random number and use it to populate fancybox images -->
           <?php 
           for ($x = 0; $x <= 3; $x++) {
+            $random = rand(1,13);      
           ?>
-            <?php
-              $random = rand(1,13);      
-            ?>
             <div class="col-sm-6 col-md-3 fancyborder">
               <a class="fancybox" rel="group" href="images/photos/<?php echo $random; ?>.jpg">
                 <img src="images/photos/<?php echo $random; ?>-tn.jpg" alt="[ Random Image ]" /> <!--filename is 1-tn.jpg, 2-tn.jpg etc -->
