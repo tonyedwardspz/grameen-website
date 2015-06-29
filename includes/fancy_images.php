b9d8cb49474d7c30740accddbc3f272beb01579a
mjcave <mjcplymuni@gmail.com>
2015-06-30 00:04:29 +0100

    Added Check to image randomiser
    Check that image has not already been rendered to the fancy box by
    saving the image number to an array

--- includes/fancy_images.php
+++ includes/fancy_images.php
@@ -1,17 +1,31 @@
 
       <!-- FANCYBOX Images -->
         <div class="row fancy-container fancymargin">
-          <!-- MJC create random number and use it to populate fancybox images -->
-          <?php 
-          for ($x = 0; $x <= 3; $x++) {
-            $random = rand(1,13);      
-          ?>
-            <div class="col-sm-6 col-md-3 fancyborder">
-              <a class="fancybox" rel="group" href="images/photos/<?php echo $random; ?>.jpg">
-                <img src="images/photos/<?php echo $random; ?>-tn.jpg" alt="[ Random Image ]" /> <!--filename is 1-tn.jpg, 2-tn.jpg etc -->
-              </a>
-            </div>
+          <!-- MJC create random number and use it to populate fancybox images.
+               ensure the random image is not already selected -->
+
           <?php
-          } 
+            $imgArr = array(); //hold image number
+            $iCnt=0;
+            // main outer loop
+            for ($x = 0; $x <= 3; $x++) {
+              // get random num if not in array, add to array and then to fancybox
+              while (1){
+                $random = rand(1,13);
+                if (! in_array($random, $imgArr)) {
+                  $imgArr[$x] = $random;
+                  $iCnt++;
+          ?>
+                  <!--render fancybox -->
+                  <div class="col-sm-6 col-md-3 fancyborder">
+                    <a class="fancybox" rel="group" href="images/photos/<?php echo $random; ?>.jpg">
+                      <img src="images/photos/<?php echo $random; ?>-tn.jpg" alt="[ Random Image ]" /> <!--filename is 1-tn.jpg, 2-tn.jpg etc -->
+                    </a>
+                  </div>
+          <?php
+                  break;  //now we've added the image break out to main outer loop
+                }//endif
+              }//end of while
+            }//end of outer loop
           ?><!-- end of footer fancy box -->
         </div> <!-- Row -->
