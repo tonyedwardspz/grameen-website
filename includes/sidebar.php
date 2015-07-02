21f0b08cfcdadc773e98514ec386eca763b678b2
Tony Edwards <tony@plymouthsoftware.com>
2015-07-02 11:25:39 +0100

    Add missing title / alt tags

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,23 +3,25 @@
         <!-- SIDEBAR -->
         <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
 
-          <div class="sidebar-module sidebar-module-inset">
+          <div class="sidebar-module sidebar-module-inset opening-hours">
             <h4>Opening Hours</h4>
             <p>Open 7 Days a week including Bank Holidays</p>
             <p>Lunch - 12.00pm to 2.00pm</p>
             <p>Evening - 5.30pm to 11.30pm</p>
-            <p><em>The Grameen Restaurant has disabled facilities</em></p>
+            <p>The Grameen Restaurant has disabled facilities</p>
           </div>
 
-          <div class="sidebar-module sidebar-module-inset">
+          <div class="sidebar-module sidebar-module-inset address">
             <h4>Contact Us</h4>
-            <ol class="list-unstyled">
-              <li><i class="fa fa-phone-square fa-3x"></i> 01736 758470</li>
-              <br>
-              <li>4 Hayle Terrace</li>
-              <li>Hayle</li>
+            <ul class="list-unstyled">
+              <li>
+                <i class="fa fa-phone-square fa-3x"></i>01736 758470
+              </li>
+              <li>4 Hayle Terrace,</li>
+              <li>Hayle,</li>
+              <li>Cornwall,</li>
               <li>TR27 4BS</li>
-            </ol>
+            </ul>
           </div>
 
           <div class="sidebar-module">
@@ -29,11 +31,11 @@
             </ol>
           </div>
 
-          <div class="sidebar-module">
+          <div class="sidebar-module sidebar-social">
             <h4>Social Media Pages</h4>
-            <ol class="list-unstyled">
+            <ul class="list-unstyled">
               <li>
-                <a href="https://plus.google.com/100199113052646253342/about" target="_blank" >
+                <a href="https://plus.google.com/100199113052646253342/about" target="_blank" title="Grameen Indian Restaurant on Google Plus">
                   <span class="fa-stack fa-2x">
                     <i class="fa fa-square-o fa-stack-2x"></i>
                     <i class="fa fa-google-plus fa-stack-1x"></i>
@@ -41,7 +43,7 @@
                 </a>
               </li>
               <li>
-                <a href="https://twitter.com/search?src=typd&q=grameen%20hayle&lang=en-gb" target="_blank" >
+                <a href="https://twitter.com/search?src=typd&q=grameen%20hayle&lang=en-gb" target="_blank" title="Grameen Indian Restaurant on Twitter">
                   <span class="fa-stack fa-2x">
                     <i class="fa fa-square-o fa-stack-2x"></i>
                     <i class="fa fa-twitter fa-stack-1x"></i>
@@ -49,24 +51,23 @@
                 </a>
               </li>
               <li>
-                <a href="https://www.facebook.com/grameen.tandoorihayle" target="_blank" >
+                <a href="https://www.facebook.com/grameen.tandoorihayle" target="_blank" title="Grameen Tandoori on Facebook">
                   <span class="fa-stack fa-2x">
                     <i class="fa fa-square-o fa-stack-2x"></i>
                     <i class="fa fa-facebook fa-stack-1x"></i>
                   </span>
                 </a>
               </li>
-
-              <li> <!-- MJC Trip Advisor Widget-->
-                <div id="TA_restaurantWidgetGreen764" class="TA_restaurantWidgetGreen">
-                  <ul id="E1unA7DvU00" class="TA_links 4FolVxVkM2SR">
-                    <li id="9EyzXXB0z" class="CKiYEJ2">
-                      <a target="_blank" href="http://www.tripadvisor.co.uk/" title="TripAdvisor"><img src="http://www.tripadvisor.co.uk/img/cdsi/partner/tripadvisor_logo_117x18-24177-2.png" title="TripAdvisor"></a>
-                    </li>
-                  </ul>
-                </div>
-              </li>
-            </ol>
+            </ul>
+            <div id="TA_restaurantWidgetGreen764" class="TA_restaurantWidgetGreen">
+              <ul id="E1unA7DvU00" class="TA_links 4FolVxVkM2SR">
+                <li id="9EyzXXB0z" class="CKiYEJ2">
+                  <a target="_blank" href="http://www.tripadvisor.co.uk/" title="TripAdvisor">
+                    <img src="http://www.tripadvisor.co.uk/img/cdsi/partner/tripadvisor_logo_117x18-24177-2.png" title="Grameen Trip Advisor">
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
 
         </div><!-- /.blog-sidebar -->
