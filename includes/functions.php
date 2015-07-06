b3f66c7deeca45f088fb109169c963b1b3c75372
Tony Edwards <tony@plymouthsoftware.com>
2015-07-06 21:47:25 +0100

    Adds three reviews (from homepage)

--- includes/functions.php
+++ includes/functions.php
@@ -15,7 +15,11 @@ function getCurrentURL($pagePath){
 
 
 function getRandomReview(){
-	$review = ["This is an awesome review","Another awesome review"];
+	$review = [
+		'"The best, and most reliable, Indian restaurant in the town. A modern interior and discrete lighting make a pleasant atmosphere." - www.gourmetbritain.com',
+		'"Everybody’s food was was delicious. I had the vegetable biryani which was fragrant and tasty and my daughter enjoyed the vegetable balti." - www.qype.co.uk',
+    '"All the dishes were beautifully presented and the waiters were attentive, but never pushy. They do take-aways too!" - www.qype.co.uk'
+	];
 
 	//MJC - When we have the reviews, randomise the element selected from the array
 	$random = rand(0,1);
