1480b8bd6511c04dcd079f19b10929b99042423e
Tony Edwards <tony@plymouthsoftware.com>
2015-07-06 10:19:33 +0100

    Fixes takeaway layout on small screens

--- takeaway.php
+++ takeaway.php
@@ -22,15 +22,11 @@ include './includes/header.php';
           <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
 
           <div class="row">
-            <div class="col-xs-6">
+            <div class="col-xs-12 download-buttons">
               <a class="btn btn-default" href="images/takeawaymenu/Grameen-takeaway-menu-dl.pdf" title="Download Takeaway Menu (PDF)" role="button" download="Grameen_Takeaway_Menu">Download Menu (PDF)</a>
-            </div>
-            <div class="col-xs-6">
               <a class="btn btn-default" href="images/takeawaymenu/grameen-takeaway-menu-dl.jpg" title="Download Takeaway Menu (PNG)" role="button" download="Grameen_Takeaway_Menu">Download Menu (PNG)</a>
             </div>
-          </div>
 
-          <div class="row">
             <div class="col-xs-6">
               <a class="fancybox" rel="group" href="images/takeawaymenu/takeaway-large-outside.jpg" title="Grameen Tandoori, Hayle, Takeaway Menu" >
                 <img src="images/takeawaymenu/takeaway-thumb-1.jpg" alt="Grameen Hayle takeaway menu 1">
